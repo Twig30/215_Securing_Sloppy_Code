f4fbb583fed8ee289bfbf1ca5f6671bbaa931270
Twig30 <immanuel.delgado@stu.vertexacademies.org>
2026-02-27 19:17:42 +0000

    continued to code module

--- runtime_errors/module.py
+++ runtime_errors/module.py
@@ -1,10 +1,24 @@
 def isdigit(string):
     digits = ["0","1","2","3","4","5","6","7","8","9"]
+    index = 0
    
    #if the string has any letters return false
-    for char in string:
+    while index <= len(string)-1:
+     number = string[index].lower()
+     print(number)
+     if number in digits:
+        count+=1
+        return True
+    
+     else: 
+        return False
+
+    index += 1
+    return count
+   
+    '''for char in string:
         if char in digits:
             return True
         if char !in digits:
-            return False
+            return False'''
    #if the string has only numbers return true
